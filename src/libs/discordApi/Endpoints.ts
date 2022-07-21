3b7cff9a2069c1e5bdbba9005875a1dcdc1bfac8
pc <vponsve@gmail.com>
2022-07-22 02:12:10 +0300

    delete Message endpoint

--- src/libs/discordApi/Endpoints.ts
+++ src/libs/discordApi/Endpoints.ts
@@ -1,3 +1,5 @@
 export const createMessage = (channel_id: string) => `/channels/${channel_id}/messages`
 
 export const typing = (channel_id: string) => `/channels/${channel_id}/typing`
+
+export const deleteMessage = (channel_id: string, message_id: string) => `/channels/${channel_id}/messages/${message_id}`
