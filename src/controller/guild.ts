8bd429ee93438c7d54469e5a0bcd6d19ee658574
pc <vponsve@gmail.com>
2022-07-27 21:31:03 +0300

    update reached characters from 5 to 10

--- src/controller/guild.ts
+++ src/controller/guild.ts
@@ -93,7 +93,7 @@ const pushCharacter = async (guildId: string, character: CharacterObject) => {
     const guild = await Guild.findOne({ guildId: guildId })
     ?? await addGuild(guildId)
 
-    if(guild.characters.length > 4) throw Error('this guild reached out the max amount of characters')
+    if(guild.characters.length > 9) throw Error('this guild reached out the max amount of characters')
 
     const characterIndex = guild.characters.findIndex((item: CharacterObject) => {
         return item.username === character.username
