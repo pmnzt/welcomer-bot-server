da7d29576808ef7f79038c387f89832c6e9caa58
pc <vponsve@gmail.com>
2022-07-30 10:24:11 +0300

    make it better

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv"
 dotenv.config()
 
-const port = 4000
+const port = process.env.PORT ?? 5000
 const token = process.env.BOT_TOKEN ?? 'no token'
 const dbURI = process.env.MONGO_URI ?? ''
 const embedsColor = 15548997
